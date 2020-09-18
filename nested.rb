ea3d6ec5476989684fbcb96c469d00191aeeb0b9
Rachelle A. Spicer <spicer.rachelle@gmail.com>
2020-09-18 19:25:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -19,7 +19,7 @@ def hopper
 end
 
 def alan_kay_is_known_for
-  alan_kay = alan_kay[:known_for[0]]
+  alan_kay = alan_kay[:known_for]
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
